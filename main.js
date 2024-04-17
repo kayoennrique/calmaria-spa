a0d0d5cc09c038c18d4750bd058f82e0b2229928
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-04-17 00:42:04 -0300

    feat(git): add the mouse listeners and click to open submenu

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 document.querySelectorAll(".header__list-item").forEach(
     (item,
-    () => {
-    // adicionar um ouvinte mouseover
-    item.addEventListener("mouseover", () => toggleSubmenu(item, true));
+        () => {
+            // add a mouseover listener
+            item.addEventListener("mouseover", () => toggleSubmenu(item, true));
 
-    // adicionar um ouvinte mouseout
-    item.addEventListener("mouseout", () => toggleSubmenu(item, false));
+            // add a mouseout listener
+            item.addEventListener("mouseout", () => toggleSubmenu(item, false));
 
-    })
+            item.addEventListener("click", () => {
+                const submenu = item.querySelector(".submenu");
+
+                const isDisplayed = submenu.style.display === "block";
+
+                toggleSubmenu(item, !isDisplayed);
+            });
+        })
 );
